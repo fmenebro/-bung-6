5a7b406bff55759454de71f3581b513cce3c04da
fmenebro <f.menebroecker@smail.uni-koeln.de>
2025-11-30 20:47:50 +0100

    Create H1_main.java

---
+++ h1/H1_main.java
@@ -0,0 +1,19 @@
+package h1;
+
+public class H1_main {
+    public static boolean isMirrorArray(int[] a, int[] b) {
+        if (a == null || b == null) return false;
+        if (a.length != b.length) return false;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[b.length - 1 - i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = {4, 3, 2, 1};
+        System.out.println(isMirrorArray(a, b)); // true
+    }
+}
